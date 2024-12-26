21b7ab03d64c8e3f797e451f0d589eb9d8702ebd
hnnzj <101939155+hnnzj@users.noreply.github.com>
2024-12-26 17:23:01 -0300

    BD Connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
-
-
 const app = express();
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://hnnzj:jo234151@cluster0.l6x6f.mongodb.net/Sushi-API?retryWrites=true&w=majority&appName=Cluster0').then((res) => {
+    console.log("Base de datos conectada")
+})
+.catch((err) => {
+    console.log(err)
+})
+
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
